2701079af19bbb71b3e063ed0c5a54f02b8ecfd6
JakaPresecnik <32019706+JakaPresecnik@users.noreply.github.com>
2018-05-24 11:00:11 -0600

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,21 @@
 import webbrowser
+""" Imports a module of functions from pythons's standard library"""
 
 class Movie():
+    """ Class for representing a movie """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube,year_released, director, writer):
+        
+        """ Initiate a Movie object 
+        Arguments explained:
+            movie_title = a string of the movie's title
+            movie_storyline = a string of a short movie plot
+            poster_image = a string containing a URL to a poster image
+            trailer_youtube = a string containing a youtube URL of the movie's trailer
+            year_release = an integer of the year the movie was released
+            director = a string of the director of the movie
+            writer = a string of screanwriter/s of the movie
+        """
+        
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -11,4 +25,7 @@ class Movie():
         self.screenwriter = writer # MODIFY this in fresh_tomatoes!
         
     def show_trailer(self):
+        
+        """ Opens trailer in a web browser """
+        
         webbrowser.open(self.trailer_youtube_url)
